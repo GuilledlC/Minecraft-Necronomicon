42d88de39755bfeabb2f370a6f0f479f08b05b0c
GuilledlC <guillermodelacalhacar@gmail.com>
2022-06-03 19:24:09 +0200

    Adjusted beer effects

--- src/main/java/net/guille_dlc/necronomicon/item/BeerItem.java
+++ src/main/java/net/guille_dlc/necronomicon/item/BeerItem.java
@@ -21,7 +21,7 @@ public class BeerItem extends DrinkableItem {
     @Override
     public void affectConsumer(ItemStack stack, Level level, LivingEntity consumer) {
         List<MobEffectInstance> effects = consumer.getActiveEffects().stream().toList();
-        int slowDur = 500, slowAmp = 1, confDur = 500, confAmp = 1;
+        int slowDur = 500, slowAmp = 1, confDur = 500, confAmp = 1, absDur = 300, absAmp = 0;
         Iterator<MobEffectInstance> effectIterator = effects.iterator();
 
         //Both effects accumulate until a max of 2 and a half minutes
@@ -33,11 +33,14 @@ public class BeerItem extends DrinkableItem {
             } else if(effect.getEffect() == MobEffects.CONFUSION) {
                 confDur = Math.min(confDur + effect.getDuration(), 3000);
                 confAmp = effect.getAmplifier();
+            } else if(effect.getEffect() == MobEffects.ABSORPTION) {
+                absDur = Math.min(absDur + effect.getDuration(), 3000);
+                absAmp = effect.getAmplifier();
             }
         }
 
         consumer.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, slowDur, slowAmp));
         consumer.addEffect(new MobEffectInstance(MobEffects.CONFUSION, confDur, confAmp));
-        consumer.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 300, 0));
+        consumer.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, absDur, absAmp));
     }
 }
