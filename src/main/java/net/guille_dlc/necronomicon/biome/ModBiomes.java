4f9415311cda76cadc146e30b0d6aa403b8c0b82
GuilledlC <guillermodelacalhacar@gmail.com>
2022-10-14 19:16:38 +0200

    Upgraded to 1.19.2

--- src/main/java/net/guille_dlc/necronomicon/biome/ModBiomes.java
+++ src/main/java/net/guille_dlc/necronomicon/biome/ModBiomes.java
@@ -1,16 +1,32 @@
 package net.guille_dlc.necronomicon.biome;
 
 import net.minecraft.world.level.biome.Biome;
-import net.minecraftforge.event.RegistryEvent;
+//import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistry;
 
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModBiomes {
+    protected static DeferredRegister<Biome> BIOME_REGISTER = DeferredRegister.create(Registry.BIOME_REGISTRY, TestMod.MOD_ID);
+
     @SubscribeEvent
-    public static void registerBiomes(RegistryEvent.Register<Biome> event) {
-        IForgeRegistry<Biome> registry = event.getRegistry();
-        registry.register(TestOverworldBiomes.lovecraftCountry().setRegistryName(TestBiomes.LOVECRAFT_COUNTRY.location()));
+    public static void registerBiomes()
+    {
+        register(TestBiomes.LOVECRAFT_COUNTRY, TestOverworldBiomes::lovecraftCountry);
+    }
+
+    public static RegistryObject<Biome> register(ResourceKey<Biome> key, Supplier<Biome> biomeSupplier)
+    {
+        return BIOME_REGISTER.register(key.location().getPath(), biomeSupplier);
     }
 }
