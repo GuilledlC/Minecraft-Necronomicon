b6dbe20eddd393623ee4c1200d9751e745af090a
GuilledlC <guillermodelacalhacar@gmail.com>
2023-10-29 19:56:33 +0100

    Fix Dimension lightning

--- src/main/java/net/guille_dlc/necronomicon/common/dimension/Dimensions.java
+++ src/main/java/net/guille_dlc/necronomicon/common/dimension/Dimensions.java
@@ -3,18 +3,15 @@ package net.guille_dlc.necronomicon.common.dimension;
 import net.guille_dlc.necronomicon.api.dimension.NecronomiconDimensions;
 import net.guille_dlc.necronomicon.common.biome.LovecraftBiomeBuilder;
 import net.minecraft.core.HolderGetter;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.dimension.LevelStem;
-import net.minecraft.world.level.levelgen.*;
-import net.minecraft.world.level.levelgen.synth.NormalNoise;
+import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 
-import java.util.List;
 import java.util.OptionalLong;
 
 public class Dimensions {
@@ -23,7 +20,7 @@ public class Dimensions {
     public static DimensionType lovecraftCountry() {
         return new DimensionType(
                 OptionalLong.empty(),
-                false,
+                true,
                 false,
                 false,
                 true,
@@ -34,9 +31,14 @@ public class Dimensions {
                 256,
                 256,
                 BlockTags.INFINIBURN_OVERWORLD,
-                new ResourceLocation("minecraft:nether"),
-                0.8F,
-                new DimensionType.MonsterSettings(false, false, UniformInt.of(0, 7), 2)
+                new ResourceLocation("minecraft:overworld"),
+                0F,
+                new DimensionType.MonsterSettings(
+                        false,
+                        false,
+                        UniformInt.of(0, 7),
+                        2
+                )
         );
     }
 
